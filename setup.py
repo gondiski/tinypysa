de4b36a9739165c8b1c9a2e8ed32315f23a1055d
Enoch Otieno <enochotieno@rocketmail.com>
2022-11-08 16:11:27 +0300

    Create setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+        name='tinypesa',
+        version='0.1.0',
+        description='An Mpesa STK push payments solution',
+        url='https://github.com/gondiski/tinypysa',
+        author='Enoch Otieno',
+        author_email='enochotieno@rocketmail.com',
+        license='BSD 2-clause',
+        packages=['tinypesa'],
+        install_requires=['requests'],
+        classifiers=[
+            'Development Status :: 1 - Development',
+            'Intended Audience :: E-commerce/Payments',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 3'
+            ],
+        )
